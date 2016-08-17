5f2d5ebc8bbc91edfbf2aeea7755f0726bfd6faa
Mary Vokicic <mvoki@esailors.de>
2016-08-17 04:51:26 +0000

    #19 Dynamically set the date of the next event

---
+++ js/wtm-meetup.js
@@ -0,0 +1,25 @@
+function WTMMeetup() {
+  this.events;
+  this.callback;
+  this.script;
+}
+
+WTMMeetup.prototype.loadAllEvents = function (callback) {
+  this.callback = callback;
+  // Use JSONP request to escape the CORS
+  this.script = document.createElement('script');
+  this.script.src = '//api.meetup.com/womentechmakershamburg/events?callback=wtmMeetup.setEvents';
+  document.querySelector('head').appendChild(this.script);
+};
+
+WTMMeetup.prototype.setEvents = function (response) {
+  this.events = response.data;
+  this.script.parentNode.removeChild(this.script);
+  this.callback();
+};
+
+WTMMeetup.prototype.getNextEvent = function () {
+  return this.events ? this.events[0] : null;
+};
+
+var wtmMeetup = new WTMMeetup();
